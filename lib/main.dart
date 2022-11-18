33a38754207b92c7975a68412a24c758a4552955
mhsumon7 <74013982+mhsumon7@users.noreply.github.com>
2022-11-18 16:12:11 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            backgroundColor: Colors.blue.shade900,
+            title: Text(
+              'Ask Me Anything',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          body: askmeanything(),
+        ),
+      ),
+    );
+
+class askmeanything extends StatefulWidget {
+  const askmeanything({Key? key}) : super(key: key);
+
+  @override
+  State<askmeanything> createState() => _askmeanythingState();
+}
+
+class _askmeanythingState extends State<askmeanything> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: Image.asset('images/ball$ballNumber.png'),
+        onPressed: () {
+          setState((){
+            ballNumber = Random().nextInt(5) +1;
+          });
+        },
+      ),
+    );
+  }
+}
